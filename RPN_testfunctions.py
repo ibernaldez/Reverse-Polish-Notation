d43d546540fb5279dca09be6cba818493d259b12
Ignacio Bernaldez <46943344+ibernaldez@users.noreply.github.com>
2021-06-08 20:00:58 -0400

    Update RPN_testfunctions.py

--- RPN_testfunctions.py
+++ RPN_testfunctions.py
@@ -1,64 +1,49 @@
 import sys
 import re
-import fileinput
-
-new_stack = []
-new_stack2 = []
 
 print("Welcome to RPN")
 print("To Signal the end of stack input, type 'Calc'")
 
-def func():
-    stacky = []
+def stack_input():
+    stdin_stack = []
+    manual_stdin_stack = []
     for line in sys.stdin:
-        new_stack.append(line.strip())
-        print(new_stack)
+        stdin_stack.append(line.strip())
+        print(stdin_stack)
         if 'Calc' == line.rstrip():
-            new_stack.remove("Calc")
-            print(new_stack)
-            if len(new_stack) > 1:
+            stdin_stack.remove("Calc")
+            print(stdin_stack)
+            if len(stdin_stack) > 1:
                 s = ""
-                s = s.join(new_stack)
-                new_stack2.append(s)
-                print(new_stack2)
-                return new_stack2
-            return new_stack
-    return new_stack
+                s = s.join(stdin_stack)
+                manual_stdin_stack.append(s)
+                print(manual_stdin_stack)
+                return manual_stdin_stack
+            return stdin_stack
+    return stdin_stack
 
 def helper_function(stack, item, first, second, operator):
     stack.remove(item)
     stack.remove(first)
     stack.remove(second)
     stack.insert(0, operator)
-    return stack
 
 def main():
     stack = []
-    new_stacker = []
-    stack = func()
-    if len(stack[0]):
-        blank = True
-    if blank == True:
-        for i in range(len(stack)):
-            sub_stack = stack[i]
-            for i in sub_stack:
-                try:
-                    i = int(i)
-                    new_stacker.append(i)
-                except ValueError:
-                    new_stacker.append(i)
-            new_stacker = math(new_stacker)
-            new_stacker.clear()
-    else:
-        for i in stack[0]:
+    new_stack = []
+    stack = stack_input()
+    for scalar in range(len(stack)):
+        sub_stack = stack[scalar]
+        for scalar in sub_stack:
             try:
-                i = int(i)
-                new_stacker.append(i)
+                scalar = int(scalar)
+                new_stack.append(scalar)
             except ValueError:
-                new_stacker.append(i)
-        new_stacker = math(new_stacker)
+                new_stack.append(scalar)
+        new_stack = process_instructions(new_stack)
+        new_stack.clear()
 
-def math(new_stacker):
+def process_instructions(new_stacker):
     for i in range(len(new_stacker)):
         for item in new_stacker:
             if item == "+":
